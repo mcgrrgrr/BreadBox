73604bdba4010468e4d6f5fa10ebd6316399f0a2
mcgrrgrr <105318201+mcgrrgrr@users.noreply.github.com>
2023-07-09 16:43:04 -0500

    Fix file names and add setAddress function to EEPromProgramEE

--- sketches/EEPromProgramEE/EEPromProgramEE.ino
+++ sketches/EEPromProgramEE/EEPromProgramEE.ino
@@ -2,6 +2,15 @@
 #define SHIFT_CLK 3
 #define SHIFT_LATCH 4
 
+void setAddress(int address) {
+  shiftOut(SHIFT_DATA, SHIFT_CLK, MSBFIRST, address >> 8);
+  shiftOut(SHIFT_DATA, SHIFT_CLK, MSBFIRST, address);
+
+  digitalWrite(SHIFT_LATCH, LOW);
+  digitalWrite(SHIFT_LATCH, HIGH);
+  digitalWrite(SHIFT_LATCH, LOW);
+}
+
 
 void setup() {
 
@@ -10,8 +19,7 @@ void setup() {
   pinMode(SHIFT_CLK, OUTPUT);
   pinMode(SHIFT_LATCH, OUTPUT);
 
-  shiftOut(SHIFT_DATA, SHIFT_CLK, MSBFIRST, 0xff);
-
+  setAdress(1234)
 }
 
 void loop() {
